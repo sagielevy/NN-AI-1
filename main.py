837650aebfd3af8f690fc5b4be49123a907f6a1f
Sagie <sagielevy21@gmail.com>
2018-07-24 12:02:41 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,9 @@
+import keras
+import pygame
+import tensorflow
+import FlapPyBird.flappy as flappy
+import os
+
+if __name__ == "__main__":
+    os.chdir(os.getcwd() + r"\FlapPyBird")
+    flappy.main()
